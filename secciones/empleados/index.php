8919e344f7741c670d606395191809b8466302c8
EstebanToloza <estebanisaiastoloza@gmail.com>
2024-03-28 16:23:56 -0300

    form agregar registro

--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,5 +1,46 @@
 <?php include("../../templates/header.php"); ?>
 
-listar empleados
+<br>
+<h3>Empleados</h3>
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">
+            Agregar registro
+        </a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">CV</th>
+                        <th scope="col">Puesto</th>
+                        <th scope="col">Fecha de ingreso</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">Juan Doe</td>
+                        <td>imagen.jog</td>
+                        <td>cv.pdf</td>
+                        <td>developer senior</td>
+                        <td>12/12/2019</td>
+                        <td>
+                            <a name="" id="" class="btn btn-primary" href="#" role="button">Carta</a>
+                            <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a>
+                            <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
+
 
 <?php include("../../templates/footer.php"); ?>
